15a66e260124ab3c96c2678f6b1aa4ec95ea15b7
AnotherMizu <otrorandomas7@gmail.com>
2026-08-05 16:49:41 -0600

    desarrollo de la logica de condicion de victoria, creacion de objetos y añadido de objetos

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,25 +1,45 @@
-const ROCK = document.getElementById("rock")
-const PAPER = document.getElementById("paper")
-const SCISSORS = document.getElementById("scissors")
-
-ROCK.addEventListener("click", chooseRock)
-PAPER.addEventListener("click", choosePaper)
-SCISSORS.addEventListener("click", chooseScissors)
-
-function chooseRock() {
-
-    alert("ESCOGISTE PIEDRA")
-    
-};
-
-function choosePaper() {
-
-    alert ("Escogiste PAPEL")
-
+const ROCK = {
+    elemento : document.getElementById("rock"),
+    "ROCK" : "T",
+    "PAPER" : "L",
+    "SCISSORS" : "W"
+}
+const PAPER= {
+    elemento : document.getElementById("paper"),
+    "ROCK" : "W",
+    "PAPER" : "T",
+    "SCISSORS" : "L"
+}
+const SCISSORS = {
+    elemento : document.getElementById("scissors"),
+    "ROCK" : "L",
+    "PAPER" : "W",
+    "SCISSORS" : "T"
 }
 
-function chooseScissors() {
+ROCK.elemento.addEventListener("click", function () {
+    game(ROCK);
+});
 
-    alert ("ESCOGISTE TIJERAS")
+PAPER.elemento.addEventListener("click", function () {
+    game(PAPER);
+});
 
+SCISSORS.elemento.addEventListener("click", function () {
+    game(SCISSORS);
+});
+
+function game(userChoose){
+    const computerChoose = computerSelection();
+    if (userChoose[computerChoose] === "L"){alert("YOU LOSE")}
+    if (userChoose[computerChoose] === "W"){alert("YOU WIN")}
+    if (userChoose[computerChoose] === "T"){alert("DRAW")}
 }
+
+function computerSelection () {
+    const computerChoose = Math.floor(Math.random() * 3);
+    if (computerChoose === 0) {return "ROCK"};
+    if (computerChoose === 1) {return "PAPER"}
+    if (computerChoose === 2) {return "SCISSORS"}
+}
+
