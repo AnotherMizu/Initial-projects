93fb1d31775d9baf9f96a949e2aeb4d1755bf2c2
AnotherMizu <otrorandomas7@gmail.com>
2026-08-07 14:30:15 -0600

    Creo los inicios de la estructura del juego

---
+++ guess-the-number/script.js
@@ -0,0 +1,16 @@
+userGuess = document.getElementById("userGuess")
+startBoton = document.getElementById("startBoton")
+
+startBoton.addEventListener("click", function(){
+    
+})
+
+function random_number(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function game() {
+
+}
