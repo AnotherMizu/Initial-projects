e4786fe80b41ede4fb5a92fd64c47bbf29a94477
AnotherMizu <otrorandomas7@gmail.com>
2026-08-10 17:25:20 -0600

    Creo la logica detras del desaparecer y aparecer de objetos ademas añado que aparezca el resultado abajo para que el usuario sea capaz de verlo

--- guess-the-number/script.js
+++ guess-the-number/script.js
@@ -1,12 +1,14 @@
 
 const startBoton = document.getElementById("startBoton");
 const userGuessInput = document.getElementById("userGuess");
-
 let computer_number;
+let currentRep = 0
+const above = "te pasaste"
+const below = "te falta"
 
 startBoton.addEventListener("click", function () {
     document.getElementById("toShow").style.display = "";
-
+    document.getElementById("toHide").style.display="none"
     computer_number = random_number(1, 100);
 
     console.log("Número secreto:", computer_number);
@@ -15,6 +17,7 @@ startBoton.addEventListener("click", function () {
 userGuessInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         game();
+
     }
 });
 
@@ -28,13 +31,27 @@ function random_number(min, max) {
 }
 
 function game() {
+    currentRep += 1
+    if (currentRep === 1){
+        document.getElementById("result").style.display="";
+
+    }
+
     const userGuess = Number(userGuessInput.value);
 
     if (userGuess === computer_number) {
         alert("YOU WIN");
     } else if (userGuess > computer_number) {
-        alert("Te pasaste");
+        hintsToTheUser(above, userGuess)
     } else {
-        alert("Te falta");
+        hintsToTheUser(below, userGuess)
     }
+}
+
+function hintsToTheUser(text, guess) {
+    const newText = document.createTextNode([text, guess] + <br>)
+    const userHints = document.getElementById("userHints")
+
+    userHints.appendChild(newText)
+    
 }
